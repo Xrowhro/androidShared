7aa0ceacf7c8757df85c0ae5f31aca328e33b312
hank6578 <hank6578@gmail.com>
2016-04-15 15:14:04 +0800

    AddDrawView

--- app/src/main/java/com/example/user/sharedpaint/MainActivity.java
+++ app/src/main/java/com/example/user/sharedpaint/MainActivity.java
@@ -11,21 +11,18 @@ import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
+    private DrawView customCanvas;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        customCanvas = (DrawView) findViewById(R.id.signature_canvas);
+    }
+
+    public void clearCanvas(View v) {
+        customCanvas.clearCanvas();
     }
 
     @Override
